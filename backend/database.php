a7c100bfc949c3b0b65c19ec37336453c5be6c42
Logan <95838299+BerinyuyAnabi@users.noreply.github.com>
2025-11-20 21:41:33 +0000

    Made corrections to the database.php file

--- backend/database.php
+++ backend/database.php
@@ -1,32 +1,30 @@
-
 <?php
 
-// Collect the data from the form
-$email = $_POST["email"];
-$password = $_POST["password"];
-$security_check = $_POST['loginbtn'];
+// Collect the data from the form (if it's a login request)
+if(isset($_POST["email"])) {
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    $security_check = $_POST['loginbtn'];
 
-// Simple security check to know if the user is coming from the login form on my website
-if(isset($security_check)){
-    echo "login button clicked \n";
-}else{
-    echo "You are not using my website to login!!";
+    // Simple security check to know if the user is coming from the login form
+    if(isset($security_check)){
+        echo "login button clicked \n";
+    }else{
+        echo "You are not using my website to login!!";
+    }
 }
 
-// connect to the database
-$servername = "localhost";
-$username = "root";
-$dbpassword = "root";
-$dbname = "QuizDB";
+// Connect to the cloud database
+$servername = "sql12.freesqldatabase.com";
+$username = "sql12808774";
+$dbpassword = "GIzq5JwhtQ";
+$dbname = "sql12808774";
+$port = 3306;
 
 // Creating a database connection
-$conn = new mysqli($servername, $username, $dbpassword, $dbname);
+$conn = new mysqli($servername, $username, $dbpassword, $dbname, $port);
 if($conn->connect_error){
     die("Connection failed: " . $conn->connect_error);
-}
-else {
-    echo "Connected successfully \n";
-    // <br>
 }
 
 ?>
